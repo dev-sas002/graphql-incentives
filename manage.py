12698aa4bcfbe5cd1a8599b089a0785c548de56c
dev-sas002 <dev-sas002@users.noreply.github.com>
2023-05-05 09:00:00 +0500

    Set up project scaffolding and dependency manifests

---
+++ manage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphql.settings")
+
+
+def main():
+    """Run administrative tasks."""
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
+else:
+    # vercel.json routes every request to this module, and @vercel/python looks
+    # for a WSGI callable named ``app``/``application``. Build it only on
+    # import so that management commands do not spin up a second WSGI stack
+    # after they finish running.
+    application = get_wsgi_application()
+    app = application
